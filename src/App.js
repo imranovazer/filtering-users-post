a7e151e13700d64a4853d4a9923bce2e0f569da0
imranovazer <57639829+imranovazer@users.noreply.github.com>
2023-05-06 15:34:07 +0400

    task finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { CardGroup, Container, Input, Card, CardHeader, CardBody, CardTitle, CardText, Button } from "reactstrap";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [selectedUser, setSelectedUser] = useState('all');
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        setUsers(res.data);
+      }
+      );
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        if (selectedUser == 'all') {
+          setPosts(res.data);
+        }
+        else {
+          const newPosts = res.data.filter(e => e.userId == selectedUser);
+          setPosts(newPosts)
+        }
+
+      })
+  }
+    ,
+    [selectedUser]
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <h1>Filtering user posts app</h1>
+        <h3>Choose user</h3>
+        <Input id="exampleSelect" name="select" type="select" onChange={(e) => setSelectedUser(e.target.value)}>
+          <option value='all'>all</option>
+          {users && users.map(e =>
+            <option value={e.id} key={e.id}>
+              {e.name}
+            </option>)}
+        </Input>
+
+        <h3>Posts</h3>
+        <div>
+          {posts && posts.map(e =>
+            <Card
+              className="my-2"
+              key={e.id}
+
+            >
+              <CardHeader>
+                Posted by {e.userId}
+              </CardHeader>
+              <CardBody>
+                <CardTitle tag="h5">
+                  {e.title}
+                </CardTitle>
+                <CardText>
+                  {e.body}
+                </CardText>
+              </CardBody>
+
+            </Card>)}
+
+
+        </div>
+      </Container>
     </div>
   );
 }
